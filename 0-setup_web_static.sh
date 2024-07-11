4727d22af28163f37739655308a5aa7318f680c4
Sadek-21 <mohamed20000327@gmail.com>
2024-07-11 15:09:49 +0100

    updt

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-# setup server for deployment web_static
-apt-get update -y
-apt-get upgrade -y
-apt-get install nginx -y
-mkdir -p /data/web_static
-mkdir -p /data/web_static/releases
-mkdir -p /data/web_static/shared
-mkdir -p /data/web_static/releases/test
-echo "test deploying web_static" > /data/web_static/releases/test/index.html
-ln -fs /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/^\tserver_name/ a\\tlocation /hbnb_static \{\n\t\talias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
+# sets up web servers with Nginx, folders, and config to deploy web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo -e "simple content\n" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown --recursive ubuntu:ubuntu /data/
+sed -i 's|^\tlocation / {|\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\n\tlocation / {|' /etc/nginx/sites-available/default
 service nginx restart
